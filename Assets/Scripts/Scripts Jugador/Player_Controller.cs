71d72402581902b297be710802d0f94075188695
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2024-09-21 17:15:11 -0300

    Mejorando el movimiento y agregando animanciones

--- Assets/Scripts/Scripts Jugador/Player_Controller.cs
+++ Assets/Scripts/Scripts Jugador/Player_Controller.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
     private Rigidbody rb;
     private Animator animator;
+    [SerializeField]private bool running;
     [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +21,57 @@ public class Player_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hor = Input.GetAxisRaw("Horizontal");
-        float ver = Input.GetAxisRaw ("Vertical");
+        EstaCorriendo();
+        movimiento();
+        controlAnimaciones();
+    }
 
-        if (hor != 0 || ver != 0)
+    private void EstaCorriendo()
+    {
+        if (Input.GetKey(KeyCode.C))
         {
-            Vector3 direction = (transform.forward * ver + 
-                                transform.right * hor).normalized;
-            rb.velocity = direction * walkSpeed;
-            animator.SetBool("Walking",true);
+            running = true;
         }
         else
         {
-            rb.velocity = Vector3.zero;
+            running = false;
+        }
+    }
+
+    private void movimiento()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        if (running)
+        {
+            rb.transform.Translate(Vector3.forward * Time.deltaTime * runSpeed * ver);
+        }
+        else
+        {
+            rb.transform.Translate(Vector3.forward * Time.deltaTime * walkSpeed * ver);
+        }
+        rb.transform.Rotate(Vector3.up, turnSpeed * hor * Time.deltaTime);
+    }
+    private void controlAnimaciones()
+    {
+        float ver = Input.GetAxisRaw("Vertical");
+        if (ver != 0)
+        {
+            if (running)
+            {
+                animator.SetBool("Running", true);
+                animator.SetBool("Walking", false);
+            }
+            else
+            {
+                animator.SetBool("Walking", true);
+                animator.SetBool("Running", false);
+            }
+        }
+        else
+        {
+            animator.SetBool("Running", false);
             animator.SetBool("Walking", false);
         }
-
     }
 }
