7dbeb543b77aab316de6de51e77b59aa3d94dab8
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2024-09-22 19:07:14 -0300

    Modificando el movimiento de la moto

--- Assets/Scripts/Scripts Vehiculo/Vehiculo_Controller.cs
+++ Assets/Scripts/Scripts Vehiculo/Vehiculo_Controller.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Vehiculo_Controller : MonoBehaviour
 {
     private Rigidbody rb;
-    [SerializeField] private float aceleracion;
-    [SerializeField] private float maxSpeed = 200;
-    [SerializeField] private float speed = 0;
-    [SerializeField] private float turnSpeed; 
+    [SerializeField] float frenar = 75;
+    [SerializeField] WheelCollider rueda1;
+    //[SerializeField] WheelCollider rueda2;
+    [SerializeField] Transform llanta1;
+    //[SerializeField] Transform llanta2;
+    public float speed = 150;
+    [SerializeField] float actualSpeed;
+    private float maxSpeed = 300;
 
     // Start is called before the first frame update
     void Start()
@@ -19,23 +23,38 @@ public class Vehiculo_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movimiento();
+        actualSpeed = (2 * Mathf.PI * rueda1.radius) * rueda1.rpm * 60/100;
+        llanta1.localEulerAngles = new Vector3(0,rueda1.steerAngle,0);
+        //llanta2.localEulerAngles = new Vector3(0, rueda2.steerAngle, 0);
+
+    }
+    private void FixedUpdate()
+    {
+        if (actualSpeed < maxSpeed)
+        {
+            rueda1.motorTorque = speed * Input.GetAxis("Vertical");
+            //rueda2.motorTorque = speed * Input.GetAxis("Vertical");
+        }
+        else
+        {
+            rueda1.motorTorque = 0;
+            //rueda2.motorTorque = 0;
+        }
+
+        rueda1.steerAngle = 40 * Input.GetAxis("Horizontal");
+        //rueda2.steerAngle = 40 * Input.GetAxis("Horizontal");
+
+        if (Input.GetAxis("Vertical") == 0)
+        {
+            rueda1.brakeTorque = frenar;
+           // rueda2.brakeTorque = frenar;
+        }
+        else
+        {
+            rueda1.brakeTorque = 0;
+            //rueda2.brakeTorque = 0;
+        }
     }
 
-    private void movimiento()
-    {
-        float hor = Input.GetAxisRaw("Horizontal");
-        float ver = Input.GetAxisRaw("Vertical");
-        speed += ver * aceleracion * Time.deltaTime * 60/100;
-        if (speed >= maxSpeed)
-        {
-            speed = maxSpeed;
-        }else if (speed <= 0)
-        {
-            speed = 0;
-        }
-        Debug.Log($"Velocidad alcanzada = {speed}");
-        rb.transform.Translate(Vector3.forward * speed);
-        rb.transform.Rotate(Vector3.up, turnSpeed * hor * Time.deltaTime);
-    }
+
 }
