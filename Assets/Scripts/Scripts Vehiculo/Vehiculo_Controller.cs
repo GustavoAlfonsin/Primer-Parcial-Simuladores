65ae053655236f1293a9a168d7628db6c105d29e
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2024-09-26 19:22:04 -0300

    Termine el movimiento de la moto

--- Assets/Scripts/Scripts Vehiculo/Vehiculo_Controller.cs
+++ Assets/Scripts/Scripts Vehiculo/Vehiculo_Controller.cs
@@ -1,60 +1,128 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Vehiculo_Controller : MonoBehaviour
 {
-    private Rigidbody rb;
-    [SerializeField] float frenar = 75;
-    [SerializeField] WheelCollider rueda1;
-    //[SerializeField] WheelCollider rueda2;
-    [SerializeField] Transform llanta1;
-    //[SerializeField] Transform llanta2;
-    public float speed = 150;
-    [SerializeField] float actualSpeed;
-    private float maxSpeed = 300;
+    [SerializeField] private float moveInput, steerInput, rayLength, currentVelocityOffset;
+    public float maxSpeed, acceleration, steerStrenght, gravity, bikeTiltIncrement = 0.9f;
+    public float zTiltAngle = 45f;
+    [HideInInspector] public Vector3 velocity;
+    [Range(1,10)]
+    public float breakingFactor;
+
+    public LayerMask derivableSurface;
+    RaycastHit hit;
+    public bool inGround;
+
+    [SerializeField] private Rigidbody sphereRB, BikeBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        sphereRB.transform.parent = null;
+        BikeBody.transform.parent = null;
+
+        rayLength = sphereRB.GetComponent<SphereCollider>().radius + 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        actualSpeed = (2 * Mathf.PI * rueda1.radius) * rueda1.rpm * 60/100;
-        llanta1.localEulerAngles = new Vector3(0,rueda1.steerAngle,0);
-        //llanta2.localEulerAngles = new Vector3(0, rueda2.steerAngle, 0);
 
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+        transform.position = sphereRB.transform.position;
+
+        velocity = BikeBody.transform.InverseTransformDirection(BikeBody.velocity);
+        currentVelocityOffset = velocity.z / maxSpeed;
+        
     }
     private void FixedUpdate()
     {
-        if (actualSpeed < maxSpeed)
+        movimiento();
+    }
+
+    private void movimiento()
+    {
+        if (Grounded())
         {
-            rueda1.motorTorque = speed * Input.GetAxis("Vertical");
-            //rueda2.motorTorque = speed * Input.GetAxis("Vertical");
+            if (!Input.GetKey(KeyCode.Space))
+            {
+                Aceleracion();
+                rotacion();
+            }
+            Brake();
         }
         else
         {
-            rueda1.motorTorque = 0;
-            //rueda2.motorTorque = 0;
+            Gravity();
         }
+        BikeTilt();
+    }
 
-        rueda1.steerAngle = 40 * Input.GetAxis("Horizontal");
-        //rueda2.steerAngle = 40 * Input.GetAxis("Horizontal");
+    private void Aceleracion()
+    {
+        sphereRB.velocity = Vector3.Lerp(sphereRB.velocity,
+                                         maxSpeed * moveInput * transform.forward,
+                                         Time.fixedDeltaTime * acceleration);
+    }
 
-        if (Input.GetAxis("Vertical") == 0)
+    private void rotacion()
+    {
+        transform.Rotate(0,steerInput * moveInput * steerStrenght * Time.fixedDeltaTime,0,Space.World);
+    }
+
+    private void Brake()
+    {
+        if (Input.GetKey(KeyCode.Space))
         {
-            rueda1.brakeTorque = frenar;
-           // rueda2.brakeTorque = frenar;
-        }
-        else
-        {
-            rueda1.brakeTorque = 0;
-            //rueda2.brakeTorque = 0;
+            sphereRB.velocity *= breakingFactor / 10;
         }
     }
 
+    private bool Grounded()
+    {
+        float radius = rayLength - 0.5f;
+        Vector3 origin = sphereRB.transform.position + radius * Vector3.up;
+        if (Physics.SphereCast(origin, radius + 0.5f, -transform.up,
+                                out hit, rayLength, derivableSurface))
+        {
+            inGround = true;
+            return true;
+        }
+        else
+        {
+            inGround = false;
+            return false;
+        }
+    }
 
+    private void Gravity()
+    {
+        sphereRB.AddForce(gravity * Vector3.down, ForceMode.Acceleration);
+    }
+
+    private void BikeTilt()
+    {
+        float xRot = (Quaternion.FromToRotation(BikeBody.transform.up, hit.normal) *
+                        BikeBody.transform.rotation).eulerAngles.x;
+
+        float zRot = 0;
+        if(currentVelocityOffset > 0)
+        {
+            zRot = -zTiltAngle * steerInput * currentVelocityOffset;
+        }
+
+        Quaternion targetRot = Quaternion.Slerp(BikeBody.transform.rotation,
+                                Quaternion.Euler(xRot,
+                                transform.eulerAngles.y, zRot),
+                                bikeTiltIncrement);
+
+        Quaternion newRotation = Quaternion.Euler(targetRot.eulerAngles.x, 
+                                transform.eulerAngles.y, targetRot.eulerAngles.z);
+
+        BikeBody.MoveRotation(newRotation);
+    }
 }
